9b010c82f352a515fe8998e7e37711f93b4e4e4e
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-02 04:54:05 -0300

    Implement random snake initial position and fix the field print function

--- snake.js
+++ snake.js
@@ -6,18 +6,39 @@ const FIELD_WIDTH = 70;
 const FIELD_HEIGHT = 20;
 const TOTAL_FIELD_HEIGHT = FIELD_HEIGHT + 2; // Field height considering the border.
 const TOTAL_FIELD_WIDTH = FIELD_WIDTH + 2; // Field width considering the border.
+const INITIAL_SNAKE_SIZE = 3;
 const HORIZONTAL_BORDER = '=';
 const VERTICAL_BORDER = '*';
 const SNAKE_SEGMENT = 'o';
 let field = [];
 
+// Get random number between 0 and 1, including both.
+function inclusiveRandom() {
+  return Math.ceil(Math.random() * 1000000000000) / 1000000000000;
+}
+
+// Get random integer between min and max, including both.
+// Adapted from:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(min, max) {
+  return Math.floor(inclusiveRandom() * (max - min) + min);
+}
+
+// Randomly set the initial position for the snake.
+function setInitialSnakePosition() {
+  let initialRow = getRandomInt(INITIAL_SNAKE_SIZE + 1, FIELD_HEIGHT + 1 - INITIAL_SNAKE_SIZE);
+  let initialCol = getRandomInt(INITIAL_SNAKE_SIZE + 1, FIELD_WIDTH + 1 - INITIAL_SNAKE_SIZE);
+
+  field[initialRow][initialCol] = SNAKE_SEGMENT;
+}
+
 // Field initialization function:
 // => Build top and bottom field borders.
 function buildFieldBorderRow() {
-  let borderRow = '';
+  let borderRow = [];
 
   for (let colIndex of Array(TOTAL_FIELD_WIDTH).keys()) {
-    borderRow += HORIZONTAL_BORDER;
+    borderRow.push(HORIZONTAL_BORDER);
   }
 
   return borderRow;
@@ -26,12 +47,13 @@ function buildFieldBorderRow() {
 // Field initialization function:
 // => Build left and right field borders.
 function buildFieldRow() {
-  let row = VERTICAL_BORDER;
+  let row = [];
+  row[0] = VERTICAL_BORDER;
 
   for (let colIndex of Array(FIELD_WIDTH).keys()) {
-    row += ' ';
+    row.push(' ');
   }
-  row += VERTICAL_BORDER;
+  row.push(VERTICAL_BORDER);
 
   return row;
 }
@@ -55,12 +77,24 @@ function initializeField() {
     row = getFieldRow(rowIndex);
     field.push(row);
   }
+  setInitialSnakePosition();
+}
+
+// Convert the row array into a string in order to print it.
+function fieldRowToString(rowArray) {
+  let rowString = '';
+
+  for (let col of rowArray) {
+    rowString += col;
+  }
+
+  return rowString;
 }
 
 // Print the current state of the field.
 function printField() {
   for (let row of field) {
-    console.log(row);
+    console.log(fieldRowToString(row));
   }
 }
 
